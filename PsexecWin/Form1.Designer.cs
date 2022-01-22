cb17e65a38acecec07b1547fac8deb1cec362d6d
mt12at <78842266+mt12at@users.noreply.github.com>
2022-01-22 13:00:54 +0900

    Добавьте файлы проекта.

---
+++ PsexecWin/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PsexecWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключиться";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(233, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(327, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            this.файлToolStripMenuItem.Click += new System.EventHandler(this.файлToolStripMenuItem_Click);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            this.настройкиToolStripMenuItem.Click += new System.EventHandler(this.настройкиToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите ip или имя ПК";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Версия 1.0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(378, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 173);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.Visible = false;
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(327, 270);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PsexecWin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ PsexecWin/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Net;
+
+namespace PsexecWin
+{
+    public partial class Form1 : Form
+    {
+        private const string FILE_NAME = "ComboBoxUPdate.txt";
+        private const string V = "\\";
+        private string ipps;
+        private bool keyerr = true;
+        public Form1()
+        {
+            InitializeComponent();
+            CreateFile(FILE_NAME);
+            UpdatedCombobox();       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CheckIp(Convert.ToString(comboBox1.Text)) == true)
+            {
+                if ((Testping(Convert.ToString(comboBox1.Text))) == true)
+                {
+                    connect(Convert.ToString(comboBox1.Text));
+                }
+                else MessageBox.Show(" Удаленный ip адрес не доступен!!! \n Проверьте правильность написания ip адреса \n Проверьте подключение к сети", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+            else
+            {
+                DNS_show(Convert.ToString(comboBox1.Text));
+                if (keyerr == true)
+                {
+                    if ((Testping(Convert.ToString(ipps))) == true)
+                    {
+                        if (CheckIp(Convert.ToString(ipps)) == true)
+                        {
+                            connect(ipps);
+                        }
+                        else MessageBox.Show(" ip не найден ", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    else MessageBox.Show(" Удаленный ip адрес не доступен!!! \n Проверьте правильность написания ip адреса \n Проверьте подключение к сети", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else MessageBox.Show(" ip не найден ", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool Testping(string x)
+        {
+                Ping png = new Ping();
+                PingReply pingReply = png.Send(x);
+                if (pingReply.Status == IPStatus.Success)
+                return true;
+                else return false;
+           
+        }
+        static bool CheckIp(string address)
+        {
+            var nums = address.Split('.');
+            int useless;
+            return nums.Length == 4 && nums.All(n => int.TryParse(n, out useless)) &&
+                   nums.Select(int.Parse).All(n => n < 256);
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Данное приложение разработано для удобства использования другого приложения 'Psexec.exe'\nЗапускает командную строку удаленного компьютера  \n \n Версия 1.1 \n 1. Исправлена ошибка, неизвестные имена \n 2. Появилась обработка нажатия клавиши “Enter” \n \n Предложение, баги и т.д. пишите. TokarevML@nornik.ru  ", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdatedCombobox()
+        {
+            using (StreamReader sr = File.OpenText(FILE_NAME))
+            {
+                String input;
+                while ((input = sr.ReadLine()) != null)
+                {
+                    comboBox1.Items.Add(input);
+                    comboBox1.Text = comboBox1.Items[0].ToString();
+                }              
+                sr.Close();
+            }
+           
+        }
+        private void SaveComboBoxText ()
+        {
+            if (ScanFile() == true)
+            {
+                using (StreamWriter sr = File.AppendText(FILE_NAME))
+                {
+                    sr.WriteLine(ReadComboBox());
+                    sr.Close();
+                }
+                UpdatedCombobox();
+            }
+
+        }
+        private string ReadComboBox ()
+        {
+            return Convert.ToString(comboBox1.Text);
+        }
+        private bool ScanFile ()
+        {
+            StreamReader sr = new StreamReader(FILE_NAME);
+            while (!sr.EndOfStream)
+            {
+                string st = sr.ReadLine();
+                if (st.StartsWith(comboBox1.Text))
+                {
+                    listBox1.Items.Add(Convert.ToString(st));
+                    return false;
+                }
+            }
+            sr.Close();
+            return true;
+        }
+
+        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void CreateFile(string curFile)
+        {
+            if (!File.Exists(curFile))
+            {
+                FileStream st = File.Create(curFile);
+                st.Close();
+            }
+        }
+        private void DNS_show (string nameps)
+        { 
+            try
+            {
+                IPHostEntry host2 = Dns.GetHostEntry(nameps);
+                foreach (IPAddress ip in host2.AddressList)
+                    ipps = ip.ToString();
+            }
+            catch (Exception t)
+            {
+                keyerr = false;
+                return;
+            }
+            keyerr = true;
+        }
+        private void connect(string ipcon)
+        {
+            string ip = Convert.ToString(V + V + ipcon);
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            psi.WorkingDirectory = @"%CD%";
+            psi.Arguments = "/k " + "psexec.exe " + ip + " cmd.exe";
+            try
+            {
+                Process.Start(psi);
+            }
+
+            catch (Exception t)
+            {
+                MessageBox.Show("Ошибка " + t, "сообщение", MessageBoxButtons.OK);
+            }
+            SaveComboBoxText();
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void настройкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пока настроек нет.\n Условия работы программы: \n 1. Нахождение файла psexec.exe в папке с программой\n 2. Иметь учетную запись администратора на удаленном ПК", "Настройки", MessageBoxButtons.OK);
+        }
+    }
+}
